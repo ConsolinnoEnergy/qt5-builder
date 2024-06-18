5349141bfb2d10991bf4dae16b5f69340a803466
l.heizinger <l.heizinger@consolinno.de>
2024-06-18 13:16:49 +0200

    Update build_ios.sh

--- build_ios.sh
+++ build_ios.sh
@@ -13,7 +13,18 @@ cd ..
 git clone https://github.com/crystalidea/qt-build-tools.git
 rsync -av qt-build-tools/5.15.14/qtbase/ qt5/qtbase
 cd qt5
-./configure QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" -opensource -confirm-license -nomake examples -nomake tests -xplatform macx-ios-clang -release -no-openssl -securetransport -prefix /Users/runner/work/qt5-builder/QtBuild
+./configure \
+    QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" \
+    -opensource \ 
+    -confirm-license \
+    -nomake examples \
+    -nomake tests \
+    -xplatform macx-ios-clang \
+    -release \
+    -no-openssl 
+    -securetransport \
+    -prefix /Users/runner/work/qt5-builder/QtBuild
+
 make -j$(sysctl -n hw.ncpu)
 make install
 cd $ROOT_DIR
