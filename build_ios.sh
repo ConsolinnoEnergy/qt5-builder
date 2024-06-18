992995e942c7898ca338b737ac782e419b5cb462
l.heizinger <l.heizinger@consolinno.de>
2024-06-18 15:57:29 +0200

    Skip qtlocation

--- build_ios.sh
+++ build_ios.sh
@@ -13,7 +13,7 @@ cd ..
 git clone https://github.com/crystalidea/qt-build-tools.git
 rsync -av qt-build-tools/5.15.14/qtbase/ qt-everywhere-src-5.15.14/qtbase
 cd qt-everywhere-src-5.15.14
-./configure QMAKE_APPLE_DEVICE_ARCHS="arm64" -opensource -confirm-license -nomake examples -nomake tests -xplatform macx-ios-clang -release -no-openssl -securetransport -prefix /Users/runner/work/qt5-builder/QtBuild
+./configure QMAKE_APPLE_DEVICE_ARCHS="arm64" -opensource -confirm-license -nomake examples -nomake tests -xplatform macx-ios-clang -release -no-openssl -securetransport -skip qtlocation -prefix /Users/runner/work/qt5-builder/QtBuild
 
 make -j$(sysctl -n hw.ncpu)
 make install
