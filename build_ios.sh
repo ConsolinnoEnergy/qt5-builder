8eb0cea2ba8af07bf179d3027eb20d133cadea0b
l.heizinger <l.heizinger@consolinno.de>
2024-06-18 11:04:11 +0200

    Try fixing qtmultimedia build issue

--- build_ios.sh
+++ build_ios.sh
@@ -6,11 +6,15 @@ cd ..
 git clone https://code.qt.io/qt/qt5.git
 cd qt5 && git checkout 5.15
 perl init-repository --module-subset=default,-qtwebengine
+# Quick and dirty patch. Should be done using a patch file, when things are running.
+# See https://decovar.dev/blog/2018/02/17/build-qt-statically/
+sed -i qtmultimedia/src/plugins/avfoundation/camera/avfcamerautility.mm -e 's/unary_function/__unary_function/'
 cd ..
 git clone https://github.com/crystalidea/qt-build-tools.git
+
 rsync -av qt-build-tools/5.15.14/qtbase/ qt5/qtbase
 cd qt5
-./configure QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" -opensource -confirm-license -nomake examples -nomake tests -no-openssl -securetransport -prefix /Users/runner/work/qt5-builder/QtBuild
+./configure QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" -opensource -confirm-license -nomake examples -nomake tests -xplatform macx-ios-clang -release -no-openssl -securetransport -prefix /Users/runner/work/qt5-builder/QtBuild
 make -j$(sysctl -n hw.ncpu)
 make install
 cd $ROOT_DIR
