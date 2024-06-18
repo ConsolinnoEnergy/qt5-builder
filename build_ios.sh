0caaf5d7213d44f2669f0d6cd4059f9ed338fdae
l.heizinger <l.heizinger@consolinno.de>
2024-06-18 14:32:32 +0200

    Update build_ios.sh

--- build_ios.sh
+++ build_ios.sh
@@ -3,14 +3,14 @@
 set -e
 ROOT_DIR=$(pwd)
 cd ..
-#wget https://download.qt.io/official_releases/qt/5.15/5.15.14/single/qt-everywhere-opensource-src-5.15.14.tar.xz
-#tar xfv qt-everywhere-opensource-src-5.15.14.tar.xz
+wget https://download.qt.io/official_releases/qt/5.15/5.15.14/single/qt-everywhere-opensource-src-5.15.14.tar.xz
+tar xfv qt-everywhere-opensource-src-5.15.14.tar.xz
 cd qt-everywhere-src-5.15.14
 # Quick and dirty patch. Should be done using a patch file, when things are running.
 # See https://decovar.dev/blog/2018/02/17/build-qt-statically/
 sed -i'' -e 's/unary_function/__unary_function/' qtmultimedia/src/plugins/avfoundation/camera/avfcamerautility.mm
 cd ..
-#git clone https://github.com/crystalidea/qt-build-tools.git
+git clone https://github.com/crystalidea/qt-build-tools.git
 rsync -av qt-build-tools/5.15.14/qtbase/ qt-everywhere-src-5.15.14/qtbase
 cd qt-everywhere-src-5.15.14
 ./configure QMAKE_APPLE_DEVICE_ARCHS="arm64" -opensource -confirm-license -nomake examples -nomake tests -xplatform macx-ios-clang -release -no-openssl -securetransport -prefix /Users/runner/work/qt5-builder/QtBuild
