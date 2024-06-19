f52029ca1bde6f410b874fafdfd7e8e88a38547e
lheiz <98517717+lheiz@users.noreply.github.com>
2024-06-19 15:35:09 +0200

    Change reference to own fork of qt-build-tools and pin ref

--- build_ios.sh
+++ build_ios.sh
@@ -10,7 +10,8 @@ cd qt-everywhere-src-5.15.14
 # See https://decovar.dev/blog/2018/02/17/build-qt-statically/
 sed -i'' -e 's/unary_function/__unary_function/' qtmultimedia/src/plugins/avfoundation/camera/avfcamerautility.mm
 cd ..
-git clone https://github.com/crystalidea/qt-build-tools.git
+git clone https://github.com/ConsolinnoEnergy/qt-build-tools.git
+cd qt-build-tools && git checkout 197da8ca95d8552a65fb8de5274cb8f475961d7d && cd ..
 rsync -av qt-build-tools/5.15.14/qtbase/ qt-everywhere-src-5.15.14/qtbase
 cd qt-everywhere-src-5.15.14
 ./configure QMAKE_APPLE_DEVICE_ARCHS="arm64" -opensource -confirm-license -nomake examples -nomake tests -xplatform macx-ios-clang -release -no-openssl -securetransport -skip qtlocation -prefix /Users/runner/work/qt5-builder/QtBuild
