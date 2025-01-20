376dff1856c0f2940c019f272d5a15ddb7d7f6c3
IT20AI <anton-iljaschenko@gmx.de>
2025-01-20 15:59:39 +0100

    Refactor build_android.sh to improve directory navigation and streamline qt-build-tools checkout process

--- build_android.sh
+++ build_android.sh
@@ -8,16 +8,11 @@ cd ..
 wget https://download.qt.io/official_releases/qt/5.15/5.15.14/single/qt-everywhere-opensource-src-5.15.14.tar.xz
 tar xfv qt-everywhere-opensource-src-5.15.14.tar.xz
 
-git clone https://github.com/ConsolinnoEnergy/qt-build-tools.git
-cd qt-build-tools
-git checkout 197da8ca95d8552a65fb8de5274cb8f475961d7d
 cd ..
-
+git clone https://github.com/ConsolinnoEnergy/qt-build-tools.git
+cd qt-build-tools && git checkout 197da8ca95d8552a65fb8de5274cb8f475961d7d && cd ..
 rsync -av qt-build-tools/5.15.14/qtbase/ qt-everywhere-src-5.15.14/qtbase
-
 cd qt-everywhere-src-5.15.14
-
-#export ANDROID_NDK_ROOT=/usr/local/lib/android/sdk/ndk-bundle
 
 ./configure \
   -platform linux-clang \ 
