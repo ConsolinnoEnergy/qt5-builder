a93ee63a25d4aa9bf878f6139b7795912a8c7ad2
IT20AI <anton-iljaschenko@gmx.de>
2025-01-21 15:57:10 +0100

    fix: update Qt version to 5.15.16 in build scripts

--- build_android.sh
+++ build_android.sh
@@ -5,12 +5,12 @@ set -e
 ROOT_DIR=$(pwd)
 
 cd ..
-wget https://download.qt.io/official_releases/qt/5.15/5.15.14/single/qt-everywhere-opensource-src-5.15.14.tar.xz
-tar xfv qt-everywhere-opensource-src-5.15.14.tar.xz
+wget https://download.qt.io/official_releases/qt/5.15/5.15.16/single/qt-everywhere-opensource-src-5.15.16.tar.xz
+tar xfv qt-everywhere-opensource-src-5.15.16.tar.xz
 
-cd qt-everywhere-src-5.15.14
+cd qt-everywhere-src-5.15.16
 
-./configure -opensource -confirm-license -release -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/21.4.7075529" -xplatform android-clang -platform linux-clang -nomake tests -nomake examples -skip qtlocation -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -release -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/21.4.7075529" -xplatform android-clang -platform linux-clang -nomake tests -nomake examples -skip qtlocation -skip qtwebengine -securetransport -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
 
 make -j"$(nproc)"
 make install
