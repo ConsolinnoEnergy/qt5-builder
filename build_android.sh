8c45c58f8af6d4d61050306385efb0fa8f8cd895
IT20AI <anton-iljaschenko@gmx.de>
2025-01-21 10:37:41 +0100

    fix: adjust streamline configure command

--- build_android.sh
+++ build_android.sh
@@ -10,19 +10,7 @@ tar xfv qt-everywhere-opensource-src-5.15.14.tar.xz
 
 cd qt-everywhere-src-5.15.14
 
-./configure 
-  -opensource \
-  -confirm-license \
-  -release \            
-  -android-sdk $ANDROID_SDK_ROOT \        
-  -android-ndk "$ANDROID_SDK_ROOT/ndk/21.4.7075529" \        
-  -xplatform android-clang \
-  -platform linux-clang \        
-  -nomake tests \
-  -nomake examples \
-  -skip qtlocation \
-  -no-warnings-are-errors \                      
-  -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -release -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/21.4.7075529" -xplatform android-clang -platform linux-clang -nomake tests -nomake examples -skip qtlocation -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
 
 make -j"$(nproc)"
 make install
