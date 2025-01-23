b9258428a6ae6b8e95ca38889c7509d6ad68f2ad
IT20AI <anton-iljaschenko@gmx.de>
2025-01-23 13:28:37 +0100

    fix: try with kde qt repo

--- build_android.sh
+++ build_android.sh
@@ -5,10 +5,10 @@ set -e
 ROOT_DIR=$(pwd)
 
 cd ..
-wget https://download.qt.io/official_releases/qt/5.15/5.15.16/single/qt-everywhere-opensource-src-5.15.16.tar.xz
-tar xfv qt-everywhere-opensource-src-5.15.16.tar.xz
+git clone --branch v5.15.16-lts-lgpl https://invent.kde.org/qt/qt/qt5.git
+cd qt5
 
-cd qt-everywhere-src-5.15.16
+perl init-repository
 
 ./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/27.2.12479018" -xplatform android-clang -nomake tests -nomake examples -skip qtlocation -skip webglplugin -qt-zlib -qt-libpng -qt-libjpeg -skip webengine -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
 
