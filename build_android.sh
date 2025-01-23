c464bf39ca231286953200c82155791cafb5e29d
IT20AI <anton-iljaschenko@gmx.de>
2025-01-23 10:27:04 +0100

    fix: update Android NDK version in build script

--- build_android.sh
+++ build_android.sh
@@ -10,8 +10,8 @@ tar xfv qt-everywhere-opensource-src-5.15.16.tar.xz
 
 cd qt-everywhere-src-5.15.16
 
-./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/21.4.7075529" -xplatform android-clang -nomake tests -nomake examples -skip qtlocation -skip webglplugin -qt-zlib -qt-libpng -qt-libjpeg -skip webengine -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/27.2.12479018" -xplatform android-clang -nomake tests -nomake examples -skip qtlocation -skip webglplugin -qt-zlib -qt-libpng -qt-libjpeg -skip webengine -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
 
-make -j"$(nproc)" install
+make -j"$(nproc)" install 
 
 cd "$ROOT_DIR"
