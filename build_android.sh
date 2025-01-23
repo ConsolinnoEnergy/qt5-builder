051b0783881fcbcedcc162eda04795c056f67707
IT20AI <anton-iljaschenko@gmx.de>
2025-01-23 14:49:37 +0100

    fix: add Qt download and extraction steps to build workflow

--- build_android.sh
+++ build_android.sh
@@ -4,13 +4,9 @@ set -e
 
 ROOT_DIR=$(pwd)
 
-cd ..
-git clone --branch v5.15.16-lts-lgpl https://invent.kde.org/qt/qt/qt5.git
 cd qt5
 
-perl init-repository
-
-./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/27.2.12479018" -xplatform android-clang -nomake tests -nomake examples -skip qtlocation -skip webglplugin -qt-zlib -qt-libpng -qt-libjpeg -skip webengine -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/21.4.7075529" -xplatform android-clang -nomake tests -nomake examples -skip qtlocation -skip webglplugin -qt-zlib -qt-libpng -qt-libjpeg -skip webengine -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
 
 make -j"$(nproc)"
 
