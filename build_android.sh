6ef9aa6ca1d8ca7984d017e0a2eb054cf01a0791
IT20AI <anton-iljaschenko@gmx.de>
2025-01-24 09:00:32 +0100

    fix: update Android build workflow to include Qt installation step

--- build_android.sh
+++ build_android.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-set -e
-
-ROOT_DIR=$(pwd)
-
 cd qt5
 
 ./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/21.4.7075529" -xplatform android-clang -nomake tests -nomake examples -skip qtlocation -skip webglplugin -qt-zlib -qt-libpng -qt-libjpeg -skip webengine -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
 
-make -j"$(nproc)"
+sudo make -j"$(nproc)"
 
-make install
-
-cd "$ROOT_DIR"
