616aa5d7aaccf0a7351101461941414cabd8db06
IT20AI <anton-iljaschenko@gmx.de>
2025-01-24 11:48:04 +0100

    fix: skip qtlocation

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,7 @@
 
 cd qt5
 
-./configure -opensource -confirm-license -release -nomake tests -nomake examples -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -release -nomake tests -nomake examples -skip qtlocation -prefix /home/runner/work/qt5-builder/QtBuild 
 
 sudo make -j"$(nproc)"
 
