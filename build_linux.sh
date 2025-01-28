61bc442a0035b3aea5bedf04f49476e9487c1629
IT20AI <anton-iljaschenko@gmx.de>
2025-01-28 10:13:16 +0100

    fix: skip additional plugins in qt5 configuration

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,7 @@
 
 cd qt5
 
-./configure -opensource -confirm-license -release -nomake tests -nomake examples -skip qtlocation -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -release -nomake tests -nomake examples -skip qtlocation -skip webglplugin -skip webengine -prefix /home/runner/work/qt5-builder/QtBuild 
 
 sudo make -j"$(nproc)"
 
