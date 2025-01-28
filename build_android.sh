58a16fdf63472cb01f3e19614b48fcba821f7df8
IT20AI <anton-iljaschenko@gmx.de>
2025-01-28 15:16:09 +0100

    fix: update scripts for Android configuration

--- build_android.sh
+++ build_android.sh
@@ -2,7 +2,7 @@
 
 cd qt5
 
-./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/21.4.7075529" -xplatform android-clang -nomake tests -nomake examples -skip qtlocation -skip webglplugin -qt-zlib -qt-libpng -qt-libjpeg -skip webengine -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/21.4.7075529" -xplatform android-clang -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
 
 sudo make -j"$(nproc)"
 
