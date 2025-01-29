95671f962fd38403b7e5ea316431bc00e0252e74
IT20AI <anton-iljaschenko@gmx.de>
2025-01-29 15:58:02 +0100

    fix: update Android build configuration and NDK setup

--- build_android.sh
+++ build_android.sh
@@ -2,7 +2,7 @@
 
 cd qt5
 
-./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/27.2.12479018" -xplatform android-clang -android-ndk-host linux-x86_64 -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-34 -android-sdk $ANDROID_SDK_ROOT -android-ndk $ANDROID_NDK_ROOT -xplatform android-clang -android-ndk-host linux-x86_64 -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
 
 sudo make -j"$(nproc)"
 
