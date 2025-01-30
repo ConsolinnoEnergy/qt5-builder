46c7114fde0a53e68754513abbef00f4fb91032b
IT20AI <anton-iljaschenko@gmx.de>
2025-01-30 10:50:12 +0100

    fix: streamline Android SDK and NDK setup in CI workflow

--- build_android.sh
+++ build_android.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-export ANDROID_NDK_ROOT=$HOME/android-ndk-r21e
-export ANDROID_SDK_ROOT=$HOME/Android/Sdk
-
-export PATH=$ANDROID_NDK_ROOT:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$PATH
-
 cd qt5
 
-./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-33 -android-sdk $ANDROID_SDK_ROOT -android-ndk $ANDROID_NDK_ROOT -xplatform android-clang -android-ndk-host linux-x86_64 -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -release --disable-rpath -android-sdk $ANDROID_SDK_ROOT -android-ndk $ANDROID_NDK_ROOT -xplatform android-clang -android-ndk-host linux-x86_64 -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
 
 sudo make -j"$(nproc)"
 
