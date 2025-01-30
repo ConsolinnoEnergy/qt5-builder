1b3c1ae49fd718beea6a84a2689da3c37cc31f9f
IT20AI <anton-iljaschenko@gmx.de>
2025-01-30 09:04:10 +0100

    fix: add path to NDK, SDK

--- build_android.sh
+++ build_android.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export ANDROID_NDK_ROOT=$HOME/android-ndk-r21e
+export ANDROID_SDK_ROOT=$HOME/Android/Sdk
+
 cd qt5
 
 ./configure -opensource -confirm-license -release --disable-rpath -android-ndk-platform android-33 -android-sdk $ANDROID_SDK_ROOT -android-ndk $ANDROID_NDK_ROOT -xplatform android-clang -android-ndk-host linux-x86_64 -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
