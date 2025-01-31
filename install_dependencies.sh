e6419f0afaaa78deb4c8f48b20e6b82c1dbe2da4
IT20AI <anton-iljaschenko@gmx.de>
2025-01-31 13:16:02 +0100

    fix: update install_dependencies.sh to improve package installation and fix broken dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,81 +1,92 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+set -e
 
 sudo apt-get update && sudo apt-get install -y \
     build-essential \
-    libgl1-mesa-dev \
-    libxkbcommon-x11-0 \
-    libxcb-render-util0 \
-    libxcb-xinerama0 \
     perl \
     python3 \
     ninja-build \
-    libbluetooth-dev \
-    libproxy-dev \
-    libssl-dev \
-    zlib1g-dev \
+    ccache \
+    flex \
+    bison \
+    gperf \
+    ruby \
+    ca-certificates \
+    libgl1-mesa-dev \
+    libglu1-mesa-dev \
+    libegl1-mesa-dev \
+    libgles2-mesa-dev \
+    mesa-common-dev \
+    libgbm-dev \
     libx11-dev \
     libxext-dev \
     libxfixes-dev \
     libxrender-dev \
-    bluez \
-    bluez-tools \
     libxi-dev \
     libxkbcommon-dev \
     libxkbcommon-x11-dev \
-    libxcb1-dev \
+    libxrandr-dev \
+    libxcursor-dev \
+    libxcomposite-dev \
+    libxdamage-dev \
+    libxtst-dev \
+    libxss-dev \
     libx11-xcb-dev \
-    libglu1-mesa-dev \
-    '^libxcb.*-dev' \
+    libxkbcommon-x11-0 \
+    libxcb1-dev \
     libxcb-keysyms1-dev \
     libxcb-image0-dev \
     libxcb-shm0-dev \
     libxcb-icccm4-dev \
     libxcb-sync-dev \
     libxcb-xfixes0-dev \
-    libglu-dev \
-    libegl1-mesa-dev \
-    libgles2-mesa-dev \
-    mesa-common-dev \
-    libgbm-dev \
+    libxcb-render-util0 \
+    libxcb-xinerama0 \
     libwayland-dev \
     libwayland-cursor0 \
     libwayland-egl1-mesa \
+    libasound2-dev \
+    libpulse-dev \
+    libgstreamer1.0-dev \
+    libgstreamer-plugins-base1.0-dev \
+    libgstreamer-plugins-good1.0-dev \
+    libgstreamer-plugins-bad1.0-dev \
+    bluez \
+    bluez-tools \
+    libbluetooth-dev \
+    libssl-dev \
+    zlib1g-dev \
+    libnss3-dev \
+    libproxy-dev \
     libfreetype6-dev \
     libfontconfig1-dev \
     libpng-dev \
     libjpeg-dev \
-    libasound2-dev \
-    libpulse-dev \
-    libproxy-dev \
-    ca-certificates \
-    libicu-dev \
-    flex \
-    bison \
-    gperf \
-    libxslt-dev \
-    ruby \
-    libxcursor-dev \
-    libxcomposite-dev \
-    libxdamage-dev \
-    libxrandr-dev \
-    libxtst-dev \
-    libxss-dev \
     libdbus-1-dev \
     libevent-dev \
     libcap-dev \
     libudev-dev \
     libpci-dev \
-    libnss3-dev \
-    libasound2-dev \
+    libicu-dev \
+    libxslt-dev \
     nodejs \
-    libgstreamer1.0-dev \
-    libgstreamer-plugins-base1.0-dev \
-    libgstreamer-plugins-good1.0-dev \
-    libgstreamer-plugins-bad1.0-dev \
     libc++1 \
     libc6-i386 \
     lib32stdc++6 \
     libclang-11-dev \
     libc++abi-11-dev \
     llvm-11 \
-    ccache
+    openjdk-8-jdk \
+    unzip \
+    wget \
+    cmake \
+    ant \
+    libstdc++6 \
+    libncurses5 \
+    libncurses5-dev \
+    libncursesw5 \
+    libncursesw5-dev
+
+# Falls es Probleme mit libc++abi-11-dev gibt, fehlende Abhängigkeiten fixen:
+sudo apt --fix-broken install -y
