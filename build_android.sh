0bf07318177717b65c946e806a4932bde7920d9a
IT20AI <anton-iljaschenko@gmx.de>
2025-02-03 10:35:40 +0100

    fix: update Android SDK setup and configure script for NDK path

--- build_android.sh
+++ build_android.sh
@@ -2,7 +2,7 @@
 
 cd qt5
 
-./configure -opensource -confirm-license -platform android-clang -xplatform android-clang -android-sdk $ANDROID_SDK_ROOT -android-ndk "$ANDROID_SDK_ROOT/ndk/21.3.6528147" -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -opengl es2 -qt-libjpeg -qt-libpng -android-ndk-host linux-x86_64 -prefix /home/runner/work/qt5-builder/QtBuild
+./configure -opensource -confirm-license -platform android-clang -xplatform android-clang -android-sdk $ANDROID_SDK_ROOT -android-ndk $ANDROID_NDK_ROOT -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -opengl es2 -qt-libjpeg -qt-libpng -android-ndk-host linux-x86_64 -prefix /home/runner/work/qt5-builder/QtBuild
 
 sudo make -j"$(nproc)"
 
