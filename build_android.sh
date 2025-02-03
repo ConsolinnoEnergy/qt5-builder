2ae5d93ceb1a37d3b6da932ac66270777d1d1a51
IT20AI <anton-iljaschenko@gmx.de>
2025-02-03 15:48:44 +0100

    fix: update Android build scripts for improved NDK host configuration and add missing dependencies

--- build_android.sh
+++ build_android.sh
@@ -3,10 +3,11 @@
 cd qt5
 
 export ANDROID_NDK_ROOT=/usr/local/lib/android/sdk/ndk/22.1.7171670
+export ANDROID_NDK_HOST=linux-x86_64
 
 #./configure -opensource -confirm-license -platform android-clang -xplatform android-clang -android-sdk $ANDROID_SDK_ROOT -android-ndk $ANDROID_NDK_ROOT -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -opengl es2 -qt-libjpeg -qt-libpng -android-ndk-host linux-x86_64 -prefix /home/runner/work/qt5-builder/QtBuild
 
-./configure -opensource -confirm-license -platform android-clang -release --disable-rpath -android-sdk $ANDROID_SDK_ROOT -android-ndk $ANDROID_NDK_ROOT -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -opengl es2 -qt-libjpeg -qt-libpng -android-ndk-host linux-x86_64 -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -opensource -confirm-license -platform android-clang -release --disable-rpath -android-sdk $ANDROID_SDK_ROOT -android-ndk $ANDROID_NDK_ROOT -nomake tests -nomake examples -skip qttranslations -skip qtserialport -no-warnings-are-errors -opengl es2 -qt-libjpeg -qt-libpng -android-ndk-host $ANDROID_NDK_HOST -prefix /home/runner/work/qt5-builder/QtBuild 
 
 sudo make -j"$(nproc)"
 
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -90,5 +90,7 @@ sudo apt-get update && sudo apt-get install -y \
     libncursesw5 \
     libncursesw5-dev
 
+sudo apt install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang 
+
 # Falls es Probleme mit libc++abi-11-dev gibt, fehlende Abhängigkeiten fixen:
 sudo apt --fix-broken install -y
