722d89c6acc1fb6bf8948e8fa8980e798f618748
IT20AI <anton-iljaschenko@gmx.de>
2025-02-04 15:23:58 +0100

    fix: add openjdk-11-jdk to dependencies installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -88,7 +88,8 @@ sudo apt-get update && sudo apt-get install -y \
     libncurses5 \
     libncurses5-dev \
     libncursesw5 \
-    libncursesw5-dev
+    libncursesw5-dev \
+    openjdk-11-jdk
 
 sudo apt install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang 
 
