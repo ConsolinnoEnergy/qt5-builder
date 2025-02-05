3141e1ad85835b8cd61db32847336681292b26f7
IT20AI <anton-iljaschenko@gmx.de>
2025-02-05 08:39:04 +0100

    fix: update Android NDK and SDK versions

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -88,8 +88,7 @@ sudo apt-get update && sudo apt-get install -y \
     libncurses5 \
     libncurses5-dev \
     libncursesw5 \
-    libncursesw5-dev \
-    openjdk-11-jdk
+    libncursesw5-dev
 
 sudo apt install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang 
 
