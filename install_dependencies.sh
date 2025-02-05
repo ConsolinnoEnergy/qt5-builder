c2d340ca44f67050bdf3bc43a7bf2d9c74ef1d4d
IT20AI <anton-iljaschenko@gmx.de>
2025-02-05 10:18:02 +0100

    fix: update Android build workflow and dependencies installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -88,9 +88,24 @@ sudo apt-get update && sudo apt-get install -y \
     libncurses5 \
     libncurses5-dev \
     libncursesw5 \
-    libncursesw5-dev
-
-sudo apt install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang 
-
-# Falls es Probleme mit libc++abi-11-dev gibt, fehlende Abhängigkeiten fixen:
-sudo apt --fix-broken install -y
+    libncursesw5-dev \
+    clang-format \
+    clang-tidy \
+    clang-tools \
+    clang clangd \
+    libc++-dev \
+    libc++1 \
+    libc++abi-dev \
+    libc++abi1 \
+    libclang-dev \
+    libclang1 \
+    liblldb-dev \
+    libllvm-ocaml-dev \
+    libomp-dev \
+    libomp5 \
+    lld \
+    lldb \
+    llvm-dev \
+    llvm-runtime \
+    llvm \
+    python3-clang
