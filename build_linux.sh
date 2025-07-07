e6db15c59cca7f7adca6a3ad4749b06902a9f678
IT20AI <anton-iljaschenko@gmx.de>
2025-07-07 12:38:33 +0200

    feat: ESUI-405 update android SDK

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,7 @@
 
 cd qt5
 
-./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -no-warnings-are-errors -skip qtlocation -skip qtmultimedia -skip qtpurchasing -skip qtwebengine -skip qtwebglplugin -prefix /home/runner/work/qt5-builder/QtBuild 
 
 sudo make -j"$(nproc)"
 
