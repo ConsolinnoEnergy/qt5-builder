69e451107d7c38f6ce845ea3d3f85247b0aeee31
IT20AI <anton-iljaschenko@gmx.de>
2025-08-07 12:48:23 +0200

    feat: removed paramater -skip webplugin from installer

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,7 @@
 
 cd qt5
 
-./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -skip qtlocation -skip webglplugin -skip webengine -skip qttranslations -skip qtserialport -no-warnings-are-errors -opengl es2 -qt-libjpeg -qt-libpng -prefix /home/runner/work/qt5-builder/QtBuild 
+./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -skip qtlocation -skip webengine -skip qttranslations -skip qtserialport -no-warnings-are-errors -opengl es2 -qt-libjpeg -qt-libpng -prefix /home/runner/work/qt5-builder/QtBuild 
 
 sudo make -j"$(nproc)"
 
