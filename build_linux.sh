f4c8a1eb599afcbeead63548779aefce74e44a7d
IT20AI <anton-iljaschenko@gmx.de>
2025-08-11 09:17:28 +0200

    set qt path variable in build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -2,6 +2,6 @@
 
 cd qt5
 
-./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -skip qtlocation -skip qtmultimedia -skip qtpurchasing -skip qtwebengine -no-warnings-are-errors -no-opengl -qt-libjpeg -qt-libpng -prefix -prefix /home/runner/work/qt5-builder/qt5-builder/Qt/5.15.16
+./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -skip qtlocation -skip qtmultimedia -skip qtpurchasing -skip qtwebengine -no-warnings-are-errors -no-opengl -qt-libjpeg -qt-libpng -prefix -prefix "/home/runner/work/qt5-builder/QtBuild"
 
 sudo make -j"$(nproc)"
