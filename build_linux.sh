87e3578dd320545bb02ac896c3e046cfc0292268
IT20AI <anton-iljaschenko@gmx.de>
2025-08-14 16:05:15 +0200

    feat: update qt to version 5.15.17

--- build_linux.sh
+++ build_linux.sh
@@ -2,6 +2,6 @@
 
 cd qt5
 
-./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -skip qtlocation -skip qtmultimedia -skip qtpurchasing -skip qtwebengine -no-warnings-are-errors -no-opengl -qt-libjpeg -qt-libpng -prefix /home/runner/work/qt5-builder/QtBuild
+./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -skip qtmultimedia -skip qtpurchasing -no-warnings-are-errors -no-opengl -qt-libjpeg -qt-libpng -prefix /home/runner/work/qt5-builder/QtBuild
 
 sudo make -j"$(nproc)"
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,15 +1,13 @@
 sudo apt-get update
 sudo apt-get install -y \
-  build-essential \
-  g++ \
-  make \
-  perl \
-  python2 \
-  git \
-  pkg-config \
-  bison \
-  flex \
-  gperf \
+  build-essential clang lld ninja-build cmake perl python3.11 pkg-config git nodejs \
+  gperf bison flex \
+  libgl1-mesa-dev libglu1-mesa-dev \
+  libx11-dev libx11-xcb-dev libxcb1-dev libxcb-util-dev \
+  libxext-dev libxfixes-dev libxrender-dev libxkbcommon-dev libxkbcommon-x11-dev \
+  libxi-dev libxrandr-dev libxcursor-dev libxinerama-dev \
+  libfontconfig1-dev libfreetype6-dev libjpeg-dev libpng-dev \
+  zlib1g-dev libssl-dev \
   libx11-dev \
   libxext-dev \
   libxi-dev \
@@ -43,16 +41,16 @@ sudo apt-get install -y \
   libwayland-dev \
   libwayland-cursor0 \
   libwayland-egl1-mesa \
+  libgles2-mesa-dev \
   '^libxcb.*-dev' \
-  libx11-xcb-dev \
-  libgl1-mesa-dev \
-  libc6 \
-  libavahi-common-dev \
-  libavahi-client-dev \
-  libwebp-dev \
-  libjpeg-dev \
-  libpng-dev \
-  libharfbuzz-dev \
+  libxcb-xinerama0-dev \
+  libnss3-dev \
+  libnspr4-dev \
+  libxss-dev \
+  libdrm-dev \
+  libgbm-dev \
   libsqlite3-dev \
-  zlib1g-dev \
-  libicu-dev 
+  libxcomposite-dev \
+  libxcursor-dev \
+  libxdamage-dev \
+  libxkbfile-dev
