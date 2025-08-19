70903e3bbaa2fe53019a94a6ce6fc1b1a2f4bac7
IT20AI <anton-iljaschenko@gmx.de>
2025-08-19 14:33:40 +0200

    feat: update params

--- build_linux.sh
+++ build_linux.sh
@@ -2,6 +2,6 @@
 
 cd qt5
 
-./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -skip qtmultimedia -skip qtpurchasing -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild
+./configure -c++std c++17 -ssl -openssl-linked -I "/home/runner/work/master/android_openssl/ssl_3/include" -L "/home/runner/work/master/android_openssl/ssl_3" -opensource -confirm-license -release -nomake tests -nomake examples -nomake qtmultimedia -nomake qtpurchasing -nomake qt3d -nomake qtgamepad -nomake qttools -no-warnings-are-errors -prefix /home/runner/work/qt5-builder/QtBuild
 
 sudo make -j"$(nproc)"
