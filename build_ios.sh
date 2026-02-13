92713873fc1ac27333846a9bd79ce43dda1a099a
Michael Schwarz <m.schwarz@consolinno.de>
2026-02-13 07:46:01 +0100

    Try to fix build error with iOS SDK version 26:
    2026-02-13T06:13:47.4422190Z In file included from png.c:14:
    2026-02-13T06:13:47.4523700Z ./pngpriv.h:524:16: fatal error: 'fp.h' file not found

--- build_ios.sh
+++ build_ios.sh
@@ -9,6 +9,8 @@ cd qt-everywhere-src-5.15.14
 # Quick and dirty patch. Should be done using a patch file, when things are running.
 # See https://decovar.dev/blog/2018/02/17/build-qt-statically/
 sed -i'' -e 's/unary_function/__unary_function/' qtmultimedia/src/plugins/avfoundation/camera/avfcamerautility.mm
+# Fix for Xcode 16.2+ where fp.h has been removed
+find . -name "pngpriv.h" -exec sed -i'' -e '/#include <fp.h>/d' {} \;
 cd ..
 git clone https://github.com/ConsolinnoEnergy/qt-build-tools.git
 cd qt-build-tools && git checkout 197da8ca95d8552a65fb8de5274cb8f475961d7d && cd ..
