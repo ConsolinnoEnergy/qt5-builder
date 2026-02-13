0f778e8fd1a25f8c2edfb22ed945ab6c2c73b451
Michael Schwarz <m.schwarz@consolinno.de>
2026-02-13 08:01:18 +0100

    Try again to fix build with iOS SDK 26

--- build_ios.sh
+++ build_ios.sh
@@ -9,13 +9,13 @@ cd qt-everywhere-src-5.15.14
 # Quick and dirty patch. Should be done using a patch file, when things are running.
 # See https://decovar.dev/blog/2018/02/17/build-qt-statically/
 sed -i'' -e 's/unary_function/__unary_function/' qtmultimedia/src/plugins/avfoundation/camera/avfcamerautility.mm
-# Fix for Xcode 16.2+ where fp.h has been removed
-find . -name "pngpriv.h" -exec sed -i'' -e '/#include <fp.h>/d' {} \;
 cd ..
 git clone https://github.com/ConsolinnoEnergy/qt-build-tools.git
 cd qt-build-tools && git checkout 197da8ca95d8552a65fb8de5274cb8f475961d7d && cd ..
 rsync -av qt-build-tools/5.15.14/qtbase/ qt-everywhere-src-5.15.14/qtbase
 cd qt-everywhere-src-5.15.14
+# Fix for Xcode 16.2+ where fp.h has been removed
+find . -name "pngpriv.h" -exec sed -i'' -e '/#include <fp.h>/d' {} \;
 ./configure QMAKE_APPLE_DEVICE_ARCHS="arm64" -opensource -confirm-license -nomake examples -nomake tests -xplatform macx-ios-clang -release -no-openssl -securetransport -skip qtlocation -prefix /Users/runner/work/qt5-builder/QtBuild
 
 make -j$(sysctl -n hw.ncpu)
