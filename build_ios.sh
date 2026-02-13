6a2f532600deb628cebac7f0822a0cb2fb873b7b
Michael Schwarz <m.schwarz@consolinno.de>
2026-02-13 08:13:12 +0100

    Try again to fix build with iOS SDK 26

--- build_ios.sh
+++ build_ios.sh
@@ -15,7 +15,8 @@ cd qt-build-tools && git checkout 197da8ca95d8552a65fb8de5274cb8f475961d7d && cd
 rsync -av qt-build-tools/5.15.14/qtbase/ qt-everywhere-src-5.15.14/qtbase
 cd qt-everywhere-src-5.15.14
 # Fix for Xcode 16.2+ where fp.h has been removed
-find . -name "pngpriv.h" -exec sed -i'' -e '/#include <fp.h>/d' {} \;
+echo "Patching pngpriv.h to remove fp.h include..."
+find . -name "pngpriv.h" -type f -print -exec sed -i'' -e '/#.*include.*<fp\.h>/d' {} \;
 ./configure QMAKE_APPLE_DEVICE_ARCHS="arm64" -opensource -confirm-license -nomake examples -nomake tests -xplatform macx-ios-clang -release -no-openssl -securetransport -skip qtlocation -prefix /Users/runner/work/qt5-builder/QtBuild
 
 make -j$(sysctl -n hw.ncpu)
